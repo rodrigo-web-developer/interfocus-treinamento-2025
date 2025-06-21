82f043ae089fcfd7afd998602d124ceefac4a8de
unknown <x10rodrigo@gmail.com>
2025-06-21 12:23:44 -0300

    adicionado debounce effect

--- Treinamento/treinamento-react/src/pages/Alunos/index.jsx
+++ Treinamento/treinamento-react/src/pages/Alunos/index.jsx
@@ -14,8 +14,17 @@ export default function AlunosPage() {
     }
 
     useEffect(() => {
-        fetchData();
-    }, [search])
+        // debounce
+        var timeout = setTimeout(() => {
+            fetchData();
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [search]);
+
+    
 
     return <div>
         <h1>Alunos</h1>
@@ -38,6 +47,9 @@ function AlunoCard({ aluno }) {
     return <div class="card">
         <h4>{aluno.nome}</h4>
         <ul>
+            <li>
+                <img height="72" src={aluno.foto}></img> 
+            </li>
             <li>
                 <strong>ID:</strong> {aluno.id}
             </li>
